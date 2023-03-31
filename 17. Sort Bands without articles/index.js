af394a79228c63843e1f9202ead1b7cb59524cf6
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-03-31 16:51:07 +0530

    sorting without articles

---
+++ 17. Sort Bands without articles/index.js
@@ -0,0 +1,18 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();     /*  const regex = /ab+c/;   */
+}
+
+/*sort(compareFn)
+compareFn : A function that defines the sort order. 
+The return value should be a number whose positivity indicates the relative order of the two elements.*/
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#bands').innerHTML =
+  sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
+
+console.log(sortedBands);
