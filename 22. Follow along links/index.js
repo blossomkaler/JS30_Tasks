3545f7fe2f0b18b21aa47432b78f53938f1f7cf5
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-04-05 13:22:57 +0530

    highlight links

--- 22. Follow along links/index.js
+++ 22. Follow along links/index.js
@@ -5,6 +5,16 @@ const triggers = document.querySelectorAll('a');
 
   function highlightLink() {
     const linkCoords = this.getBoundingClientRect();
+/* 
+element.getBoundingClientRect() returns a DOMRect object providing information 
+about the size of an element and its position relative to the viewport.
+
+the returned value is a DOMRect object which is the smallest rectangle which contains the entire element, 
+including its padding and border-width. The left, top, right, bottom, x, y, width, and height properties 
+describe the position and size of the overall rectangle in pixels. 
+Properties other than width and height are relative to the top-left of the viewport.
+*/
+
     console.log(linkCoords);
     const coords = {
       width: linkCoords.width,
@@ -20,3 +30,6 @@ const triggers = document.querySelectorAll('a');
   }
 
   triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+/* mouseenter event is fired at an Element when a pointing device (usually a mouse) 
+is initially moved so that its hotspot is within the element at which the event was fired.  */
+
